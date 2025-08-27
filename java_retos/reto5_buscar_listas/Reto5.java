a8f40edcbeb5d27381c335cce326fcee17a58fc2
hsbarreray <hsbarreray@academia.usbbog.edu.co>
2025-08-26 22:22:03 -0500

    Update Reto5.java

--- java_retos/reto5_buscar_listas/Reto5.java
+++ java_retos/reto5_buscar_listas/Reto5.java
@@ -9,7 +9,7 @@ import java.util.regex.*;
 
 public class Reto5 {
     public static void main(String[] args) {
-        String texto = "Las listas son [1, 2, 3] y [4, 5, 6].";
+        String texto = "Las listas son [5, 2, 1] y [7, 3, 8].";
         // Expresión regular para listas de números
         String patron = "\\[\\s*\\d+(?:\\s*,\\s*\\d+)*\\s*\\]";
         Pattern pattern = Pattern.compile(patron);
